c6b4915019428b60d1a77b87956114f1c0d9ace6
luigi <lotito.lg@gmail.com>
2021-04-02 18:30:26 +0200

    modifiche view sensor

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -26,7 +26,7 @@ class SensorsController < ApplicationController
 
     respond_to do |format|
       if @sensor.save
-        format.html { redirect_to @sensor, notice: "Sensor was successfully created." }
+        format.html { redirect_to @sensor, notice: 'Sensor was successfully created.' }
         format.json { render :show, status: :created, location: @sensor }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class SensorsController < ApplicationController
   def update
     respond_to do |format|
       if @sensor.update(sensor_params)
-        format.html { redirect_to @sensor, notice: "Sensor was successfully updated." }
+        format.html { redirect_to @sensor, notice: 'Sensor was successfully updated.' }
         format.json { render :show, status: :ok, location: @sensor }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,19 @@ class SensorsController < ApplicationController
   def destroy
     @sensor.destroy
     respond_to do |format|
-      format.html { redirect_to sensors_url, notice: "Sensor was successfully destroyed." }
+      format.html { redirect_to sensors_url, notice: 'Sensor was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sensor
-      @sensor = Sensor.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_sensor
+    @sensor = Sensor.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def sensor_params
-      params.require(:sensor).permit(:URI, :sensor_type, :public, :latitude, :longitude, :firmware, :notifica_down, :tdown, :measure_unit, :user_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def sensor_params
+    params.require(:sensor).permit(:URI, :sensor_type, :public, :latitude, :longitude, :firmware, :notifica_down, :tdown, :measure_unit, :user_id)
+  end
 end
