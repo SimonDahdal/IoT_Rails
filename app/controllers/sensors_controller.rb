f72dca056da4c505f0790917cf3ff0d41200e996
veron1208 <marcover1997@gmail.com>
2021-04-08 14:43:49 +0200

    upload file

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -61,6 +61,14 @@ class SensorsController < ApplicationController
     end
   end
 
+  def upload
+    uploaded_file = params[:firmware_file]
+    File.open(Rails.root.join('public', 'uploads', uploaded_file.original_filename), 'wb') do |file|
+      file.write(uploaded_file.read)
+    end
+  end
+
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_sensor
