1678dff2df21de10939bd251edb74b247aac7e51
veron1208 <marcover1997@gmail.com>
2021-04-08 17:30:57 +0200

    upload file 2

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -66,7 +66,18 @@ class SensorsController < ApplicationController
     File.open(Rails.root.join('public', 'uploads', uploaded_file.original_filename), 'wb') do |file|
       file.write(uploaded_file.read)
     end
+    sensor=Sensor.find(params[:sensor_id])
+    sensor.firmware=uploaded_file.original_filename
+    if sensor.save!
+      redirect_to sensor_path(params[:sensor_id]), notice: 'Firmware was successfully updated.'
+    else
+      redirect_to sensor_path(params[:sensor_id]), notice: 'Firmware wasn\'t successfully updated.'
+    end
   end
+  def download
+    send_file("#{Rails.root}/public/uploads/#{params[:firmware]}")
+  end
+
 
 
   private
