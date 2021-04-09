7269b508ccec6d1d5730157b65823515e6ae5bf9
luigi <lotito.lg@gmail.com>
2021-04-09 17:15:57 +0200

    adattamento della view e dei controller alle nested resources

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,7 +1,8 @@
 class MeasurementsController < ApplicationController
-  before_action :authenticate_user!, except: [:index_measurements_public_sensor, :create]
+  before_action :authenticate_user!, except: [:index_measurements_public_sensor, :create_by_sensor]
+  before_action :get_sensor, except: [:create_by_sensor]
   before_action :set_measurement, only: %i[ show edit update destroy ]
-  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create_by_sensor]
   # GET /measurements or /measurements.json
   #
   # def index
@@ -9,7 +10,7 @@ class MeasurementsController < ApplicationController
   # end
 
   def index
-    @measurements = Measurement.index_by_sensor(params[:sensor_id])
+    @measurements = @sensor.measurements
   end
 
   def index_measurements_sensor
@@ -33,7 +34,7 @@ class MeasurementsController < ApplicationController
 
   # GET /measurements/new
   def new
-    @measurement = Measurement.new
+    @measurement = @sensor.measurements.build
   end
 
   # GET /measurements/1/edit
@@ -42,21 +43,18 @@ class MeasurementsController < ApplicationController
 
   # POST /measurements or /measurements.json
   def create
-    sid = Sensor.where(URI: measurement_params[:sensor_uri]).pluck(:id).first
-    mp1 = measurement_params.merge(sensor_id: sid)
-    mp2 = mp1.extract!(:sensor_uri)
-    @measurement = Measurement.new(mp1)
+    @measurement = @sensor.measurements.build(measurement_params)
+    create_respond
+  end
 
-
-    respond_to do |format|
-      if @measurement.save
-        format.html { redirect_to @measurement, notice: "Measurement was successfully created." }
-        format.json { render :show, status: :created, location: @measurement }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @measurement.errors, status: :unprocessable_entity }
-      end
-    end
+  def create_by_sensor
+    #sid = Sensor.where(URI: measurement_params[:sensor_uri]).pluck(:id).first
+    #mp1 = measurement_params.merge(sensor_id: sid)
+    #mp2 = mp1.extract!(:sensor_uri)
+    #@measurement = Measurement.new(mp1)
+    @sensor = Sensor.where(URI: measurement_params[:sensor_uri]).first
+    @measurement = @sensor.measurements.build(measurement_params.except(:sensor_uri))
+    create_respond
   end
 
   # PATCH/PUT /measurements/1 or /measurements/1.json
@@ -64,8 +62,8 @@ class MeasurementsController < ApplicationController
     mp1 = measurement_params
     mp2 = mp1.extract!(:sensor_uri)
     respond_to do |format|
-      if @measurement.update(mp1)
-        format.html { redirect_to @measurement, notice: "Measurement was successfully updated." }
+     if @measurement.update(mp1)
+        format.html { redirect_to sensor_measurements_path(@sensor), notice: "Measurement was successfully updated." }
         format.json { render :show, status: :ok, location: @measurement }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -74,11 +72,12 @@ class MeasurementsController < ApplicationController
     end
   end
 
+
   # DELETE /measurements/1 or /measurements/1.json
   def destroy
     @measurement.destroy
     respond_to do |format|
-      format.html { redirect_to measurements_url, notice: "Measurement was successfully destroyed." }
+      format.html { redirect_to sensor_measurements_path(@sensor), notice: "Measurement was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -87,11 +86,28 @@ class MeasurementsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_measurement
-      @measurement = Measurement.find(params[:id])
+      @measurement = @sensor.measurements.find(params[:id])
+      #@measurement = Measurement.find(params[:id])
+    end
+
+    def get_sensor
+      @sensor = Sensor.find(params[:sensor_id])
     end
 
     # Only allow a list of trusted parameters through.
     def measurement_params
       params.require(:measurement).permit(:timestamp, :value, :sensor_uri, :sensor_id)
     end
+
+  def create_respond
+    respond_to do |format|
+      if @measurement.save
+        format.html { redirect_to sensor_measurements_path(@sensor), notice: "Measurement was successfully created." }
+        format.json { render :show, status: :created, location: @measurement }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @measurement.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :sensors do
-    resources :measurements, shallow: true
+    resources :measurements
   end
   devise_for :users
   get 'home/index'
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get 'sensor/:sensor_id/measurements', to: 'measurements#index_measurements_sensor'
   get 'public_sensors', to: 'sensors#public_sensors_index'
   get 'pub_sensor/:sensor_id/measurements', to: 'measurements#index_measurements_public_sensor'
+
+  post '/measurements.json', to: 'measurements#create_by_sensor'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
