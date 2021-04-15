7f3d44ba2504da7f7398660f2821c5e62706e444
Simon Dahdal <simon.dahdal70@hotmail.com>
2021-04-15 17:19:36 +0200

    Sistemato il Problema di stretched link. Sistemata la view e i controller dei sensori publici e i vari bottoni

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
 
   # get 'sensor/:sensor_id/measurements', to: 'measurements#index_measurements_sensor'
-  get 'sensors/:sensor_id/measurements/:format', to: 'measurements#index'
+  #get 'sensors/:sensor_id/measurements/:format', to: 'measurements#index'
 
   get 'public_sensors', to: 'sensors#public_sensors_index'
   get 'pub_sensor/:sensor_id/measurements', to: 'measurements#index_measurements_public_sensor'
